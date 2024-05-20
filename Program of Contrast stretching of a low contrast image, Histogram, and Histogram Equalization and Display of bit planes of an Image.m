fdc5b16ac2df0882cf85eed5d3111bb89ae1ae06
Praneet <92793042+Praneet0327@users.noreply.github.com>
2024-05-20 19:04:12 +0530

    Update Program of Contrast stretching of a low contrast image, Histogram, and Histogram Equalization and Display of bit planes of an Image.m

--- Program of Contrast stretching of a low contrast image, Histogram, and Histogram Equalization and Display of bit planes of an Image.m
+++ Program of Contrast stretching of a low contrast image, Histogram, and Histogram Equalization and Display of bit planes of an Image.m
@@ -7,7 +7,7 @@ I=imread('Image Path');
 subplot(4,2,1); imshow(I); title('Original Image'); 
 g=rgb2gray(I); 
 subplot(4,2,5); imshow(g); title('Gray Image'); 
-J=imadjust(g,[0.3 0.7],[]); 
+J=imadjust(g,[0.3 0.7],[]);
 subplot(4,2,3); imshow(J); title('Enhanced Image'); 
 D= imadjust(I,[0.2 0.3 0; 0.6 0.7 1],[]); 
 subplot(4,2,4);imshow(D);title('Enhanced Image 2'); 
